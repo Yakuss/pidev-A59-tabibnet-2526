593c74e12773a66167d083465061450c36cc8fc2
feriel <ferielhmaidi@gmail.com>
2026-04-12 13:26:11 +0100

    code

---
+++ src/main/java/edu/connexion3a77/tests/MainClass.java
@@ -0,0 +1,20 @@
+package edu.connexion3a77.tests;
+
+import edu.connexion3a77.entities.Personne;
+import edu.connexion3a77.services.PersonneService;
+import edu.connexion3a77.tools.MyConnection;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        //MyConnection mc = new MyConnection();
+        Personne p = new Personne("Tounsi","Abdelhamid");
+        PersonneService ps = new PersonneService();
+       // ps.addEntity2(p);
+        System.out.println(ps.getData());
+
+        MyConnection m1 = MyConnection.getInstance();
+        MyConnection m2 = MyConnection.getInstance();
+        System.out.println(m1.hashCode() +" - "+m2.hashCode());
+    }
+}   
